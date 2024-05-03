2fe2e3147a713056670d78b7bb2f784722b004f4
kornzixzac <620112418044@bru.ac.th>
2024-05-03 13:40:26 +0700

    update

--- lib/src/widgets/editor_tool_bar.dart
+++ lib/src/widgets/editor_tool_bar.dart
@@ -30,81 +30,74 @@ class EditorToolBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 54.0,
-      child: Column(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Flexible(
-            child: ListView(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              children: [
-                TabButton(
-                  tooltip: 'Insert Image',
-                  icon: Icons.image,
-                  onTap: () async {
-                    var link = await showDialog(
-                      context: context,
-                      builder: (_) {
-                        return InsertImageDialog();
-                      },
-                    );
-                    if (link != null) {
-                      if (getImageUrl != null && link[2]) {
-                        link[0] = await getImageUrl!(File(link[0]));
-                      }
-                      await javascriptExecutor.insertImage(
-                        link[0],
-                        alt: link[1],
-                      );
-                    }
+          TabButton(
+            tooltip: 'Insert Image',
+            icon: Icons.image,
+            onTap: () async {
+              var link = await showDialog(
+                context: context,
+                builder: (_) {
+                  return InsertImageDialog();
+                },
+              );
+              if (link != null) {
+                if (getImageUrl != null && link[2]) {
+                  link[0] = await getImageUrl!(File(link[0]));
+                }
+                await javascriptExecutor.insertImage(
+                  link[0],
+                  alt: link[1],
+                );
+              }
+            },
+          ),
+          Visibility(
+            visible: enableVideo!,
+            child: TabButton(
+              tooltip: 'Insert video',
+              icon: Icons.video_call_sharp,
+              onTap: () async {
+                var link = await showDialog(
+                  context: context,
+                  builder: (_) {
+                    return InsertImageDialog(isVideo: true);
                   },
-                ),
-                Visibility(
-                  visible: enableVideo!,
-                  child: TabButton(
-                    tooltip: 'Insert video',
-                    icon: Icons.video_call_sharp,
-                    onTap: () async {
-                      var link = await showDialog(
-                        context: context,
-                        builder: (_) {
-                          return InsertImageDialog(isVideo: true);
-                        },
-                      );
-                      if (link != null) {
-                        if (getVideoUrl != null && link[2]) {
-                          link[0] = await getVideoUrl!(File(link[0]));
-                        }
-                        await javascriptExecutor.insertVideo(
-                          link[0],
-                          fromDevice: link[2],
-                        );
-                      }
-                    },
-                  ),
-                ),
-                TabButton(
-                  tooltip: 'Strike through',
-                  icon: Icons.format_strikethrough,
-                  onTap: () async {
-                    await javascriptExecutor.setStrikeThrough();
-                  },
-                ),
-                TabButton(
-                  tooltip: 'Superscript',
-                  icon: Icons.superscript,
-                  onTap: () async {
-                    await javascriptExecutor.setSuperscript();
-                  },
-                ),
-                TabButton(
-                  tooltip: 'Subscript',
-                  icon: Icons.subscript,
-                  onTap: () async {
-                    await javascriptExecutor.setSubscript();
-                  },
-                ),
-              ],
+                );
+                if (link != null) {
+                  if (getVideoUrl != null && link[2]) {
+                    link[0] = await getVideoUrl!(File(link[0]));
+                  }
+                  await javascriptExecutor.insertVideo(
+                    link[0],
+                    fromDevice: link[2],
+                  );
+                }
+              },
             ),
+          ),
+          TabButton(
+            tooltip: 'Strike through',
+            icon: Icons.format_strikethrough,
+            onTap: () async {
+              await javascriptExecutor.setStrikeThrough();
+            },
+          ),
+          TabButton(
+            tooltip: 'Superscript',
+            icon: Icons.superscript,
+            onTap: () async {
+              await javascriptExecutor.setSuperscript();
+            },
+          ),
+          TabButton(
+            tooltip: 'Subscript',
+            icon: Icons.subscript,
+            onTap: () async {
+              await javascriptExecutor.setSubscript();
+            },
           ),
         ],
       ),
